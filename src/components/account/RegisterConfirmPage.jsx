fc63cb6b8b421317b3cbd8a9e81b3f79b0648fa7
Milos Petrovic <milos.brugi@gmail.com>
2019-01-11 13:09:22 -0500

    Fixing confirm messages.

--- src/components/account/RegisterConfirmPage.jsx
+++ src/components/account/RegisterConfirmPage.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const RegisterConfirmPage = (props) => {
     return (
         <div className='center'>
-            <h2>Thank you for registering to Household Planner!</h2>
+            <h2>Thank you for registering for Household Planner!</h2>
             <br/>
-            <h4>Please check your email and follow link to activate your account.</h4>
+            <h4>Please check your email and follow the link to activate your account.</h4>
         </div>
     );
 }
